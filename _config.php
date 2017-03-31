3d0dcbf58328cb8266b1ba14ccc76d7373b6d715
Jay Richardson <jay@werkbot.com>
2017-03-31 11:35:01 -0400

    - Added composer file - remove mailchimp submodule (now installed with composer in vendor folder)

--- _config.php
+++ _config.php
@@ -1,9 +1,9 @@
 <?php
-	define('MAILCHIMP_PATH',    realpath(dirname(__FILE__)));
-	define('MAILCHIMP_INCLUDES', MAILCHIMP_PATH.'/mailchimp-api');
-	define('CONSTANTCONTACT_INCLUDES', MAILCHIMP_PATH.'/constantcontact-api');
+	define('NEWSLETTER_PATH',    realpath(dirname(__FILE__)));
+	define('MAILCHIMP_INCLUDES', '../vendor/drewm/mailchimp-api/');
+	define('CONSTANTCONTACT_INCLUDES', NEWSLETTER_PATH.'/constantcontact-api');
 	//
 	SiteConfig::add_extension('NewsletterSettings');
-	//THIS WILL ADD OUR FUNCTIONALITY TO ALL PAGES IN THE SITE 
+	//THIS WILL ADD OUR FUNCTIONALITY TO ALL PAGES IN THE SITE
 	Page_Controller::add_extension('NewsletterPageExtender');
 ?>
