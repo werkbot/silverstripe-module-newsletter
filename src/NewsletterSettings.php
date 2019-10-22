efd509191a5aeb658cd4215793f1f43be1634c8a
Jay Richardson <jay@werkbot.com>
2019-10-22 17:04:34 -0400

    Added campaign monitor as an option
    - list can be specified to save the emails too

--- src/NewsletterSettings.php
+++ src/NewsletterSettings.php
@@ -1,5 +1,7 @@
 <?php
 /**/
+use SilverStripe\Core\Environment;
+use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use SilverStripe\Forms\OptionsetField;
@@ -9,29 +11,45 @@ use SilverStripe\ORM\DataExtension;
 class NewsletterSettings extends DataExtension {
 	/**/
 	private static $db = [
-		'NewsletterAPI' => "Enum('constantcontact,mailchimp,none', 'none')",
+		'NewsletterAPI' => "Enum('campaignmonitor,mailchimp,none', 'none')",
 		"NewsletterSuccessText" => "HTMLText",
 		"NewsletterErrorText" => "HTMLText",
 		'NewsletterFormButtonText' => "Text",
+
+    'CampaignMonitorListID' => "Text",
 	];
 	/**/
   public function updateCMSFields(FieldList $fields) {
-		/**/
+
 		$fields->findOrMakeTab('Root.Newsletter', 'Newsletter');
-		//PAGE LAYOUT
+
 		$fields->addFieldToTab(
 			"Root.Newsletter",
 			OptionsetField::create(
 				'NewsletterAPI',
 				'Select your newsletter API',
 				array(
-					/*'constantcontact' => 'Constant Contact',
-					'mailchimp' => 'Mail Chimp',*/
+					'campaignmonitor' => 'Campaign Monitor',
 					'none' => 'No API'
 				),
 				'none'
 			)
 		);
+
+    // Campaign Monitor
+    if(Environment::getEnv('CAMPAIGNMONITOR_API_KEY') && Environment::getEnv('CAMPAIGNMONITOR_CLIENT_ID')){
+      $auth = array('api_key' => Environment::getEnv('CAMPAIGNMONITOR_API_KEY'));
+      $wrap = new CS_REST_Clients(Environment::getEnv('CAMPAIGNMONITOR_CLIENT_ID'), $auth);
+      $result = $wrap->get_lists();
+      //echo "<pre>";print_r($result);die();
+			$listarray = array();
+      foreach($result->response as $list){
+        $listarray[$list->ListID] = $list->Name;
+      }
+      $CampaignMonitorListID = DropdownField::create("CampaignMonitorListID", "Select a list", $listarray)
+        ->displayIf("NewsletterAPI")->isEqualTo("campaignmonitor")->end();
+  		$fields->addFieldToTab('Root.Newsletter', $CampaignMonitorListID);
+    }
 
 		//BUTTON TEXT
 		$fields->addFieldToTab("Root.Newsletter", TextField::create("NewsletterFormButtonText", "Button Text"));
