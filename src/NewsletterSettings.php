c2a8e798a47c3b4b8ebe2a9c79901472792a8d18
Jay Richardson <jay@werkbot.com>
2021-05-14 10:45:20 -0400

    Added mailchimp functionality (#11)

--- src/NewsletterSettings.php
+++ src/NewsletterSettings.php
@@ -1,13 +1,13 @@
 <?php
 /**/
-use SilverStripe\Core\Environment;
-use SilverStripe\Forms\DropdownField;
-use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
-use SilverStripe\Forms\OptionsetField;
-use SilverStripe\Forms\TextField;
-use SilverStripe\ORM\DataExtension;
 use Ctct\ConstantContact;
+use MailchimpMarketing\ApiClient;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Core\Environment;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\OptionsetField;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 /**/
 class NewsletterSettings extends DataExtension {
 	/**/
@@ -17,6 +17,7 @@ class NewsletterSettings extends DataExtension {
 		"NewsletterErrorText" => "HTMLText",
 
     'CampaignMonitorListID' => "Text",
+    'MailchimpListID' => "Text",
     'ConstantContactListID' => "Text",
     'ActiveCampaignListID' => "Text"
 	];
@@ -32,6 +33,7 @@ class NewsletterSettings extends DataExtension {
 				'Select your newsletter API',
 				array(
 					'campaignmonitor' => 'Campaign Monitor',
+					'mailchimp' => 'Mail Chimp',
 					'constantcontact' => 'Constant Contact',
 					'activecampaign' => 'Active Campaign',
 					'none' => 'No API'
@@ -45,8 +47,7 @@ class NewsletterSettings extends DataExtension {
       $auth = array('api_key' => Environment::getEnv('CAMPAIGNMONITOR_API_KEY'));
       $wrap = new CS_REST_Clients(Environment::getEnv('CAMPAIGNMONITOR_CLIENT_ID'), $auth);
       $result = $wrap->get_lists();
-      //echo "<pre>";print_r($result);die();
-	  $listarray = array();
+	    $listarray = array();
       foreach($result->response as $list){
         $listarray[$list->ListID] = $list->Name;
       }
@@ -55,11 +56,28 @@ class NewsletterSettings extends DataExtension {
   		$fields->addFieldToTab('Root.Newsletter', $CampaignMonitorListID);
     }
 
+    // Mailchimp
+    if(Environment::getEnv('MAILCHIMP_API_KEY') && Environment::getEnv('MAILCHIMP_SERVER_PREFIX')){
+      $mailchimp = new ApiClient();
+      $mailchimp->setConfig([
+        'apiKey' => Environment::getEnv('MAILCHIMP_API_KEY'),
+        'server' => Environment::getEnv('MAILCHIMP_SERVER_PREFIX')
+      ]);
+      $response = $mailchimp->lists->getAllLists();
+      $listarray = array();
+      foreach($response->lists as $list){
+        $listarray[$list->id] = $list->name;
+      }
+      $MailchimpListID = DropdownField::create("MailchimpListID", "Select a list", $listarray)
+        ->displayIf("NewsletterAPI")->isEqualTo("mailchimp")->end();
+  		$fields->addFieldToTab('Root.Newsletter', $MailchimpListID);
+    }
+
     // Constant Contact
     if(Environment::getEnv('CONSTANTCONTACT_API_KEY') && Environment::getEnv('CONSTANT_CONTACT_ACCESS_TOKEN')){
       $cc = new ConstantContact(Environment::getEnv('CONSTANTCONTACT_API_KEY'));
       $lists = $cc->getLists(Environment::getEnv('CONSTANT_CONTACT_ACCESS_TOKEN'));
-	  $listarray = array();
+	    $listarray = array();
       foreach($lists as $list){
         $listarray[$list->id] = $list->name;
       }
@@ -69,22 +87,21 @@ class NewsletterSettings extends DataExtension {
     }
 
     // Active Campaign
-    if(Environment::getEnv('ACTIVECAMPAIGN_API_KEY') && Environment::getEnv('ACTIVECAMPAIGN_URL')){  
-	  // Active Campaign List Select
-	  $ac = new ActiveCampaign(Environment::getEnv('ACTIVECAMPAIGN_URL'), Environment::getEnv('ACTIVECAMPAIGN_API_KEY'));
-	  // Adjust the default cURL timeout
-	  $ac->set_curl_timeout(10);
-	  $params = [
-	    'ids'  => 'all',
-	  ];
-	  $lists = $ac->api( "list/list_", $params );
-	  $listarray = [];
-	  foreach($lists as $list){
-	    if(is_object($list)){
-	      $listarray[$list->id] = $list->name;
-	    }
-	  }
-
+    if(Environment::getEnv('ACTIVECAMPAIGN_API_KEY') && Environment::getEnv('ACTIVECAMPAIGN_URL')){
+      // Active Campaign List Select
+      $ac = new ActiveCampaign(Environment::getEnv('ACTIVECAMPAIGN_URL'), Environment::getEnv('ACTIVECAMPAIGN_API_KEY'));
+      // Adjust the default cURL timeout
+      $ac->set_curl_timeout(10);
+      $params = [
+        'ids'  => 'all',
+      ];
+      $lists = $ac->api( "list/list_", $params );
+      $listarray = [];
+      foreach($lists as $list){
+        if(is_object($list)){
+          $listarray[$list->id] = $list->name;
+        }
+      }
       $ActiveCampaignListID = DropdownField::create("ActiveCampaignListID", "Select a list", $listarray)
         ->displayIf("NewsletterAPI")->isEqualTo("activecampaign")->end();
   		$fields->addFieldToTab('Root.Newsletter', $ActiveCampaignListID);
